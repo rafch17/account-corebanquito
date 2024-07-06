db360fbe8cf31e34f39e58e88346a93fefddd615
Sebastian Salazar <sasalazar10@espe.edu.ec>
2024-07-06 15:41:48 -0500

    Generacion de proyecto account

---
+++ account/src/main/java/core/banquito/core/account/AccountApplication.java
@@ -0,0 +1,13 @@
+package core.banquito.core.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+}
