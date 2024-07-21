e8ac7c00bde0842fef4e33df999d751f44a3bee3
nmvivas <nmvivas@espe.edu.ec>
2024-07-20 20:55:03 -0500

    Delete project

--- src/main/java/com/banquito/core/account/repository/AccountInterestAccruedRepository.java
+++
@@ -1,8 +0,0 @@
-package com.banquito.core.account.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.banquito.core.account.model.AccountInterestAccrued;
-
-public interface AccountInterestAccruedRepository extends JpaRepository<AccountInterestAccrued, Long> {
-
-}
--- src/main/java/com/banquito/core/account/repository/DebitCardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.banquito.core.account.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.banquito.core.account.model.DebitCard;
-
-public interface DebitCardRepository extends JpaRepository<DebitCard, Long> {
-
-}
