80dc9f54c8c2bad25655f1a19e20b1d97c681a99
Charles Zambrano <charleszambranoespe@outlook.com>
2024-08-02 20:01:41 -0500

    [ADD] Swagger anotations, dockerfile, docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
+# docker build -t account .
+# docker-compose down
+# docker-compose up -d --build
+
 FROM eclipse-temurin:21-jre-alpine
-COPY build/libs/account-0.1.jar account-0.1.jar
-ENTRYPOINT [ "java", "-jar", "account-0.1.jar" ]
+COPY build/libs/account-V1.jar /app/account-V1.jar
+ENTRYPOINT [ "java", "-jar", "/app/account-V1.jar" ]
 EXPOSE 8080
 RUN apk --update --no-cache add curl
-HEALTHCHECK --interval=30s --timeout=10s --start-period=5s --retries=3 \
+HEALTHCHECK --interval=1m --timeout=30s --start-period=5s --retries=3 \
     CMD curl -f http://localhost:8080/actuator/health || exit 1
 LABEL version="0.1" \
     description="Account microservice using Postgres\
--- src/main/java/com/banquito/core/account/controller/AccountTransactionController.java
+++ src/main/java/com/banquito/core/account/controller/AccountTransactionController.java
@@ -1,10 +1,7 @@
 package com.banquito.core.account.controller;
 
-import com.banquito.core.account.dto.AccountTransactionDTO;
-import com.banquito.core.account.dto.ResponseTransactionDTO;
-import com.banquito.core.account.model.AccountTransaction;
-import com.banquito.core.account.service.AccountTransactionService;
-import com.banquito.core.account.util.mapper.AccountTransactionMapper;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +11,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.GET, RequestMethod.POST,
-    RequestMethod.PUT })
+import com.banquito.core.account.dto.AccountTransactionDTO;
+import com.banquito.core.account.dto.ResponseTransactionDTO;
+import com.banquito.core.account.model.AccountTransaction;
+import com.banquito.core.account.service.AccountTransactionService;
+import com.banquito.core.account.util.mapper.AccountTransactionMapper;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT })
 @RestController
 @RequestMapping("/api/v1/account-transactions")
+@Tag(name = "Account Transaction Controller", description = "API for managing account transactions")
 public class AccountTransactionController {
 
-    private AccountTransactionService transactionService;
+    private final AccountTransactionService transactionService;
     private final AccountTransactionMapper transactionMapper;
 
     public AccountTransactionController(AccountTransactionService transactionService,
@@ -31,18 +36,18 @@ public class AccountTransactionController {
         this.transactionMapper = transactionMapper;
     }
 
+    @Operation(summary = "Create a new transaction")
     @PostMapping
     public ResponseEntity<ResponseTransactionDTO> createTransaction(@RequestBody AccountTransactionDTO dto) {
         try {
-            System.out.println("DTO recibido: " + dto);
             ResponseTransactionDTO dtoTr = this.transactionService.processTransaction(dto);
             return ResponseEntity.ok(dtoTr);
         } catch (RuntimeException rte) {
-            rte.printStackTrace();
             return ResponseEntity.badRequest().build();
         }
     }
 
+    @Operation(summary = "Get transactions by codeInternalAccount")
     @GetMapping("/{codeInternalAccount}")
     public ResponseEntity<List<AccountTransactionDTO>> getTransactionsByCodeUniqueAccount(
             @PathVariable String codeInternalAccount) {
